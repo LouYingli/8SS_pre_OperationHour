a17b80abbc9506cb7a23266f501b068e49639334
my name <myemailid@domain.comyilo3298@colorado.edu>
2020-07-27 19:02:04 -0600

    first commit

--- main.py
+++ main.py
@@ -19,12 +19,10 @@ pathway = os.getcwd()
 # list all the inputs which can be modify 
 # define the climate zones that need to be considered
 climate = ['1A','2A','2B','3A','3B','3C','4A','4B','4C','5A','5B','6A','6B','7A','8A']# define the needed climate zones
-# define the needed climate zones
-# schedule information(15 min intervel) exclude design day schedule
 
 # number of samples for training and testing meta-models
 #number of samples in each climate zone = num_sample * number of sensitive model inputs
-num_sample = 20
+num_sample = 6
 # parameters' library
 # list the entire sets of some inputs
 ###############################################################################
@@ -49,13 +47,13 @@ for cz in climate:
     
     ## record the data in the folder './results/samples'
     ## store the information of data_set
-    with open('./results/samples/data_set_'+cz+'.csv', 'wb') as csvfile:
+    with open('./results/samples/data_set.csv', 'wb') as csvfile:
         for row in data_set:
             data = csv.writer(csvfile, delimiter=',')
             data.writerow(row)
     
     ## store the information of param_values
-    with open('./results/samples/param_values_'+cz+'.csv', 'wb') as csvfile:
+    with open('./results/samples/param_values.csv', 'wb') as csvfile:
         for row in param_values:
             data = csv.writer(csvfile, delimiter=',')
             data.writerow(row)
@@ -66,6 +64,6 @@ for cz in climate:
 import parallelSimuMeta as ps
 for cz in climate:
     model_results,run_time = ps.parallelSimu(cz,1)
-rmtree('./Model/update_models')
+#rmtree('./Model/update_models')
 print run_time
 
